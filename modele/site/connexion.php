89b8e166e287ca880126484114b303d364f4c4fb
Louka Hanuise <loukahanuise@hotmail.com>
2014-11-10 10:51:28 +0100

    test

--- modele/site/connexion.php
+++ modele/site/connexion.php
@@ -1,146 +1,48 @@
 <?php
 session_start();
 include("../connexion_bdd.php");
-
-  if(isset($_SESSION['utilisateur']) && $_SESSION['motDePasse'])
-
+if(isset($_SESSION['utilisateur']) && $_SESSION['motDePasse'])
 {
-
-
-
-
-
-    echo "deja co";
-
-  
-
-
-
-  
-
-  }
-
-  
-
-  elseif (isset($_POST['utilisateur']) AND $_POST['utilisateur']!="" AND isset($_POST['motDePasse']) AND $_POST['motDePasse']!="")  
-
-
-
+  echo "deja co";
+}
+elseif (isset($_POST['utilisateur']) AND $_POST['utilisateur']!="" AND isset($_POST['motDePasse']) AND $_POST['motDePasse']!="")  
+{
+  $mail= $_POST['utilisateur'];
+  $req=$bdd->query("SELECT motDePasse FROM t_membres WHERE email='$mail'");
+  while( $donnees= $req->fetch())
   {
-
-   $mail= $_POST['utilisateur'];
-
-
-
-
-
-      $req=$bdd->query("SELECT motDePasse FROM t_membres WHERE email='$mail'");
-
-    while( $donnees= $req->fetch())
-
-     {
-
-       $passhash=$donnees['motDePasse'];
-
-      }
-
-
-
-      if (isset($passhash) && $passhash!="") 
-
-      {
-
-        
-
-      }
-
-      else
-
-      {
-
-        echo "Compte inexistant";
-
-        exit;
-
-      }
-
-
-
-      if (password_verify( $_POST['motDePasse'] , $passhash))
-
-      {
-
-        //echo "bon mdp";
-
-
-
-         $_SESSION['utilisateur']=$_POST['utilisateur'];
-
-         $_SESSION['motDePasse']=$passhash;
-
-         
-
-         header('location:../../index.php');
-
-      }
-
-      else
-
-      {
-
-        echo "Mot de passe erroné";
-
-      }
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
+    $passhash=$donnees['motDePasse'];
+  }
+  if (isset($passhash) && $passhash!="") 
+  {
+  }
+  else
+  {
+    echo "Compte inexistant";
+    exit;
+  }
+  if (password_verify( $_POST['motDePasse'] , $passhash))
+  {
+    $_SESSION['utilisateur']=$_POST['utilisateur'];
+    $_SESSION['motDePasse']=$passhash;
+    header('location:../../index.php');
+  }
+  else
+  {
+    echo "Mot de passe erroné";
+  }
 /*
-
-
-
-     $req = $bdd->prepare('SELECT * FROM clients WHERE mail=:mail AND pass=:pass'); 
-
-    $reponse = $req->execute(
-
-      array('mail' => $_POST['utilisateur'],
-
-            'pass' => $_POST['motDePasse']
-
-            ));
-
-           $donnees = $req->fetch();  
-
-
-
-     if($donnees)
-
-
-
-     { 
-
-      
-
-      
-
-      foreach ($donnees as $key => $value) 
-
-      {
-
-        $_SESSION[$key]=$value;
+  $req = $bdd->prepare('SELECT * FROM clients WHERE mail=:mail AND pass=:pass'); 
+  $reponse = $req->execute(
+  array('mail' => $_POST['utilisateur'],
+  'pass' => $_POST['motDePasse']
+  ));
+  $donnees = $req->fetch();  
+  if($donnees)
+  { 
+    foreach ($donnees as $key => $value) 
+    {
+      $_SESSION[$key]=$value;
 
         
 
@@ -165,25 +67,10 @@ include("../connexion_bdd.php");
     }
 
  */
-
-
-
   }
-
-
-
-
-
   else 
-
   {
-
-   echo "erreur manque paramétre";
-
+    echo "erreur manque paramétre";
     exit;
-
   }
-
-
-
 ?>
