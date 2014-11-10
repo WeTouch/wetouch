9fa481b43a19ad7c483746dbe49b13073b12b497
Jules Duvivier <jules.duvivier@y-nov.com>
2014-11-10 10:38:18 +0100

    UPLOAD PHOTO + BDD

--- wetouch (2).sql
+++ wetouch (2).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 03 Novembre 2014 à 12:29
+-- Généré le :  Lun 10 Novembre 2014 à 10:37
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -23,6 +23,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `t_chat`
+--
+
+CREATE TABLE IF NOT EXISTS `t_chat` (
+  `id_user1` int(11) NOT NULL,
+  `id_user2` int(11) NOT NULL,
+  `msg` mediumtext NOT NULL,
+  PRIMARY KEY (`id_user1`,`id_user2`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `t_membres`
 --
 
@@ -36,22 +49,23 @@ CREATE TABLE IF NOT EXISTS `t_membres` (
   `preference` varchar(20) DEFAULT NULL,
   `genre` varchar(20) DEFAULT NULL,
   `description` varchar(255) NOT NULL,
-  `photo_id` int(11) NOT NULL,
+  `photo_id` varchar(250) NOT NULL,
   `motDePasse` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ix_photo` (`photo_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `t_membres`
 --
 
 INSERT INTO `t_membres` (`id`, `email`, `firstname`, `name`, `dob`, `position`, `preference`, `genre`, `description`, `photo_id`, `motDePasse`) VALUES
-(1, 'jules.duvivier@y-nov.com', 'Jules', 'Duvivier', '1997-01-13', 'Biarritz', NULL, '1', 'Zboub', 0, 'zboubdeouf'),
-(2, 'fd@fdp', 'fsr', 'gf', '0005-05-15', '', NULL, 'Monsieur', '', 0, '$2y$11$hGOOhmEne0Egr//0PidNPO./EdQER7CgoTyOxQXJlSvZWigsiF1mC'),
-(3, 'damien.bellet@y-nov.com', 'Damien', 'Bellet', '1995-06-20', '', NULL, 'Monsieur', '', 0, '$2y$11$pwvgZilMTVpKxRJKbKSPoeleBEAwV70wkk0i9qH0oVLmECC89ZTEa'),
-(4, 'damien.bellet@y-nov.com', 'Damien', 'Bellet', '1995-06-20', '', NULL, 'Monsieur', '', 0, '$2y$11$kkx4/.HlRW3vRBYYn25WyeyzIijU8cipoQpZfXAAnxkQdHsGdBo5G'),
-(5, 'zboub@zboub.com', 'Michel', 'Zboub', '1997-12-13', '', NULL, 'Monsieur', '', 0, '$2y$11$YuyLn6DD67ZcVGrGRU7vQ.CfP5fMRqXtJ5fxuRNEZ9dEPKrqjSU4a');
+(1, 'jules.duvivier@y-nov.com', 'Jules', 'Duvivier', '1997-01-13', 'Biarritz', NULL, '1', 'Zboub', '0', 'zboubdeouf'),
+(2, 'fd@fdp', 'fsr', 'gf', '0005-05-15', '', NULL, 'Monsieur', '', '0', '$2y$11$hGOOhmEne0Egr//0PidNPO./EdQER7CgoTyOxQXJlSvZWigsiF1mC'),
+(3, 'damien.bellet@y-nov.com', 'Damien', 'Bellet', '1995-06-20', '', NULL, 'Monsieur', '', '0', '$2y$11$pwvgZilMTVpKxRJKbKSPoeleBEAwV70wkk0i9qH0oVLmECC89ZTEa'),
+(4, 'damien.bellet@y-nov.com', 'Damien', 'Bellet', '1995-06-20', '', NULL, 'Monsieur', '', '0', '$2y$11$kkx4/.HlRW3vRBYYn25WyeyzIijU8cipoQpZfXAAnxkQdHsGdBo5G'),
+(5, 'zboub@zboub.com', 'Michel', 'Zboub', '1997-12-13', '', NULL, 'Monsieur', '', '0', '$2y$11$YuyLn6DD67ZcVGrGRU7vQ.CfP5fMRqXtJ5fxuRNEZ9dEPKrqjSU4a'),
+(6, 'jules.duvivier@y-nov.com', 'admin', 'admin', '1997-01-13', '', NULL, 'Mademoiselle', '', '6/1.jpg', '$2y$11$oMceR6Wb695H8aub4FTqAuuHyd4lW5fkrfcfULJX0z5N5aTjQTLfy');
 
 -- --------------------------------------------------------
 
@@ -62,8 +76,18 @@ INSERT INTO `t_membres` (`id`, `email`, `firstname`, `name`, `dob`, `position`, 
 CREATE TABLE IF NOT EXISTS `t_photo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `path` varchar(255) NOT NULL,
+  `id_membres` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `t_photo`
+--
+
+INSERT INTO `t_photo` (`id`, `path`, `id_membres`) VALUES
+(1, 'photo.jpg', 4),
+(2, 'zboub.jpg', 4),
+(6, '6/1.jpg', 6);
 
 -- --------------------------------------------------------
 
