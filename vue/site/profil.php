a88ff3a25263e89e561f9a24b9fe4d50f8b6df66
pmloikju <damienbellet1@gmail.com>
2014-11-17 11:26:09 +0100

    k

--- vue/site/profil.php
+++ vue/site/profil.php
@@ -32,10 +32,58 @@ $req3=$bdd->query("SELECT * FROM t_membres WHERE id = '$match[$k]'");
 <form method="post" action="modele/site/chat.php">
 <input type="text" name="chat">
 <input type="text" name="id">
-<input type="submit" name="text" value="Send">
+<input type="button" name="text" onclick="send()" value="envoyer">
 		</form>
 
 
 
 <?php include("includes/footer.php"); ?>
-      
+
+<script type="javascript">
+	
+	function send()
+{
+  chat= document.getElementById("chat").value;
+ id= document.getElementById("id").value;
+
+var xhr;
+//if (window.XMLHttpRequest)
+ // {
+  xhr=new XMLHttpRequest();
+  //}
+xhr.onreadystatechange=function test()
+  {
+  if (xhr.readyState==4 && xhr.status==200)
+    {
+    document.getElementById("ok").innerHTML=xhr.responseText;
+    }
+  }
+xhr.open("POST","chat.php",true);
+xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+xhr.send('config='+config+  '&id='+id );
+
+} 
+
+function actu()
+{
+  chat= document.getElementById("chat").value;
+ id= document.getElementById("id").value;
+
+var xhr;
+//if (window.XMLHttpRequest)
+ // {
+  xhr=new XMLHttpRequest();
+  //}
+xhr.onreadystatechange=function test()
+  {
+  if (xhr.readyState==4 && xhr.status==200)
+    {
+    document.getElementById("ok").innerHTML=xhr.responseText;
+    }
+  }
+xhr.open("POST","chat.php",true);
+xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+xhr.send('config='+config+  '&id='+id );
+
+} 
+</script>      
