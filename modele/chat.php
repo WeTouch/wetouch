ec45dac1f877a3b7b2b87f28ca00afcb7cc167d7
Jules Duvivier <jules.duvivier@y-nov.com>
2014-11-27 10:09:57 +0100

    Affichage des discussion + jointure id/prenom

--- modele/chat.php
+++ modele/chat.php
@@ -3,20 +3,23 @@ class Chat{
   public function __construct()
   {
     $this->MatchYet=array();
+    $this->NameMatch = array();
     $this->Message=array();
+
   }
   public function getTalker()
   {
 
     require 'class/bdd.php';
-    $req = $cnx->prepare("SELECT vote FROM t_result WHERE voter =:id AND result = 1 AND vote IN ( SELECT voter FROM t_result WHERE vote=:id AND result =1)");
+    $req = $cnx->prepare("SELECT r.vote, m.firstname FROM t_result r, t_membres m  WHERE (r.voter =:id AND r.result = 1 AND r.vote IN ( SELECT r.voter FROM t_result r WHERE r.vote=:id AND r.result =1)) AND r.vote = m.id");
     $req->execute(array('id'=>$_SESSION['id']));
     while($idMatch=$req->fetch())
     {
       $this->MatchYet[]=$idMatch['vote'];
+      $this->NameMatch[]=$idMatch['firstname'];
     }
 
-    return $this->MatchYet;
+    return array($this->MatchYet , $this->NameMatch);
 
   }
 
@@ -38,25 +41,34 @@ class Chat{
   public function displayMatchForChat()
   {
     require 'class/bdd.php';
+    $i = 0;
     foreach ($this->MatchYet as $key) {
+      echo "<h4>Discussion avec " . $this->NameMatch[$i] . "</h4>";
       unset($this->Message);
-      echo $key . "<br/>";
-      $req = $cnx->prepare("SELECT message FROM t_chat WHERE (SendId =:id AND ReceiveId = :key) OR (SendId=:key AND ReceiveId = :id)");
+      $req = $cnx->prepare("SELECT c.message,m.firstname FROM t_chat c , t_membres m WHERE ((c.SendId =:id AND c.ReceiveId = :key) OR (c.SendId=:key AND c.ReceiveId = :id)) AND c.SendId = m.id  ");
       $req->execute(array('id'=>$_SESSION['id'],'key'=>$key));
       while ($message=$req->fetch())
       {
-        $this->Message[]=$message['message'];
-
+        $this->Message[] =[($message['firstname']) => ($message['message'])];
       }
 
 
-      foreach($this->Message as $msg)
+      foreach($this->Message as $valeur => $msg)
       {
-        echo "- ". $msg . "<br/>";
+        foreach($msg as $nom => $msgEnv)
+        {
+          echo $nom . " : " . $msgEnv . "<br/>";
+        }
       }
-      echo "<br/><br/>";
 
+      echo '
+      <form method="post" action="#">
+      <input type="text" name="message"/>
+      <input type="submit" value="Envoyer"/>
+      </form>';
+      $i++;
     }
+
   }
 }
 
