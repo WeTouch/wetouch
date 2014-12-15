721f156d1ed3c7fcad59e9deabee6655b952027c
Jules Duvivier <jules.duvivier@y-nov.com>
2014-12-15 11:09:49 +0100

    Chat mise en forme + fonction display

--- modele/chatValidator.php
+++ modele/chatValidator.php
@@ -7,8 +7,8 @@ class chatValidator {
   {
     require 'class/bdd.php';
     $date = date("Y-m-d H:i:s");
-    $q = array('SendId'=>$_SESSION['id'], 'ReceiveId'=>$idChat, 'message'=>$msg);
-    $req = $cnx->prepare("INSERT INTO t_chat (SendId, ReceiveId, message) VALUES (:SendId,:ReceiveId,:message)");
+    $q = array('SendId'=>$_SESSION['id'], 'ReceiveId'=>$idChat, 'message'=>$msg,'datee'=>$date);
+    $req = $cnx->prepare("INSERT INTO t_chat (SendId, ReceiveId, message, datee) VALUES (:SendId,:ReceiveId,:message,:datee)");
     $req->execute($q);
     $q = array('SendId'=>$_SESSION['id'], 'ReceiveId'=>$idChat, 'message'=>"No msg yet");
     $req = $cnx->prepare("DELETE FROM t_chat WHERE ((SendId=:SendId OR SendId=:ReceiveId) AND (ReceiveId=:SendId OR ReceiveId=:ReceiveId) AND message=:message)");
--- view/chat.php
+++ view/chat.php
@@ -6,35 +6,49 @@ if ($this->data['tabMsg']=="")
 }
 else
 {
-foreach ($this->data['tabMsg'] as $perso => $zboub)
+	foreach ($this->data['tabMsg'] as $perso => $zboub)
+	{
+		$pos = stripos($perso,"*");
+		$id = substr($perso,$pos+1,strlen($perso));
+
+		echo "<div class='all'><div class='discussion'  onclick='Afficher(this)'><div class='afid' style='display:none;'>$id</div><h3 > Discussion avec " . substr($perso,0,$pos) . "</h3></div><br/><div class='Chat $id'><div id='afid2'  style='display:none;'>$id</div>";
+
+		foreach($zboub as $message)
+		{
+			foreach($message as $sender => $msg)
+			{
+
+				if ($msg == "No msg yet")
+				{
+					echo "<h5> Pas encore de message ! </h5>";
+				}
+				else
+				{
+					echo "<h5><b>" . $sender . " : </b>" . $msg . "<br/>";
+				}
+
+			}
+
+		}
+		echo'<form method="post" action="#">
+		<input type="text" name="id" value=' . $id . ' style="width:20px; display:none;">
+		<input type="text" name="message"/>
+		<input type="submit" value="Envoyer"/>
+		</form></div></div>';
+	}
+
+}
+?>
+
+<script>
+function Afficher(el)
 {
-	     $pos = stripos($perso,"*");
-	     $id = substr($perso,$pos+1,strlen($perso));
 
-echo "<h3> Discussion avec " . substr($perso,0,$pos) . "</h3><br/>";
-  foreach($zboub as $message)
-  {
-    foreach($message as $sender => $msg)
-    {
-			
-    if ($msg == "No msg yet")
-    {
-    	echo "<h5> Pas encore de message ! </h5>";
-    }
-    else
-    {
-    	echo "<h5><b>" . $sender . " : </b>" . $msg . "<br/>";
-    }
+var zb2 = el.getElementsByClassName('afid')[0].innerHTML;
+var zb = document.getElementsByClassName('Chat ' + zb2)[0];
+var s = zb.style.display;
+zb.style.display = (s == 'block') ? 'none' : 'block' ;
 
-    }
-
-  }
-  echo'<form method="post" action="#">
-<input type="text" name="id" value=' . $id . ' style="width:20px; display:none;">
-<input type="text" name="message"/>
-<input type="submit" value="Envoyer"/>
-</form>';
-}
 
 }
- ?>
+</script>
