4ced189986f2825d4c93a8558dc2dfcaf12fadcc
Louka Hanuise <loukahanuise@hotmail.com>
2015-01-13 16:51:19 +0100

    Correction modif profil

--- view/ModifProfilMore.php
+++ view/ModifProfilMore.php
@@ -13,22 +13,22 @@
 
     <h5><b>Genre : </b> <?= $this->data['tab']['0']['preference'] ?></h5>
 
-    <a href="<?php echo $app->urlFor('Modifprofil');?>">Modifier mes informations personnelles</a>
+    <a href="<?php echo $app->urlFor('Modifprofil');?>" class="btn btn-default">Modifier mes informations personnelles</a>
   </div>
   <div id="rightP">  <h3> A propos de moi </h3>
     <form action="" method="POST">
-    <h5><b>Taille : </b><input type="text" name="taille" value=" <?= $this->data['tabMore']['0']['taille'] ?>"> cm</h5>
-    <h5><b>Poids : </b> <input type="text" name="poids" value=" <?= $this->data['tabMore']['0']['poids'] ?>"> kg</h5>
-    <h5><b>Couleur des cheveux : </b> <input type="text" name="couleurCheveux" value=" <?= $this->data['tabMore']['0']['couleurCheveux'] ?>"></h5>
-    <h5><b>Couleur des yeux : </b><input type="text" name="couleurYeux" value=" <?= $this->data['tabMore']['0']['couleurYeux'] ?>"></h5>
-    <h5><b>Bijoux corporelles : </b> <input type="text" name="bijoux" value=" <?= $this->data['tabMore']['0']['bijoux'] ?>"></h5>
-    <h5><b>Fumeur : </b>  <input type="text" name="fumeur" value=" <?= $this->data['tabMore']['0']['fumeur'] ?>"></h5>
-    <h5><b>Origine : </b>  <input type="text" name="origine" value=" <?= $this->data['tabMore']['0']['origine'] ?>"></h5>
-    <h5><b>Formation : </b>  <input type="text" name="formation" value=" <?= $this->data['tabMore']['0']['formation'] ?>"></h5>
-    <h5><b>Situation professionnelle : </b>  <input type="text" name="situation" value=" <?= $this->data['tabMore']['0']['situation'] ?>"></h5>
-    <h5><b>Statut sentimental : </b>  <input type="text" name="statut" value=" <?= $this->data['tabMore']['0']['statut'] ?>"></h5>
-    <h5><b>Que cherches-tu ? : </b>  <input type="text" name="cherche" value=" <?= $this->data['tabMore']['0']['cherche'] ?>"></h5>
-    <h5><b>Que fais-tu durant ton temps libre ? :  <input type="text" name="libre" value=" <?= $this->data['tabMore']['0']['libre'] ?>"></h5>
+    <h5><b>Taille : </b><input type="text" name="taille" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['taille']; } ?>"> cm</h5>
+    <h5><b>Poids : </b> <input type="text" name="poids" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['poids']; } ?>"> kg</h5>
+    <h5><b>Couleur des cheveux : </b> <input type="text" name="couleurCheveux" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['couleurCheveux']; } ?>"></h5>
+    <h5><b>Couleur des yeux : </b><input type="text" name="couleurYeux" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['couleurYeux']; } ?>"></h5>
+    <h5><b>Bijoux corporelles : </b> <input type="text" name="bijoux" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['bijoux']; } ?>"></h5>
+    <h5><b>Fumeur : </b>  <input type="text" name="fumeur" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['fumeur']; } ?>"></h5>
+    <h5><b>Origine : </b>  <input type="text" name="origine" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['origine']; } ?>"></h5>
+    <h5><b>Formation : </b>  <input type="text" name="formation" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['formation']; } ?>"></h5>
+    <h5><b>Situation professionnelle : </b>  <input type="text" name="situation" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['situation']; } ?>"></h5>
+    <h5><b>Statut sentimental : </b>  <input type="text" name="statut" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['statut']; } ?>"></h5>
+    <h5><b>Que cherches-tu ? : </b>  <input type="text" name="cherche" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['cherche'] ; }?>"></h5>
+    <h5><b>Que fais-tu durant ton temps libre ? :  <input type="text" name="libre" value=" <?php if(sizeof($this->data['tabMore'])) { echo $this->data['tabMore']['0']['libre']; } ?>"></h5>
   </form>
   <input type="submit" value="Modifier"/>
   </div>
