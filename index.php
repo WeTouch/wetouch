8298c52a3c072831acf824b1330b0baf40e2b0fe
Jules Duvivier <jules.duvivier@y-nov.com>
2015-01-13 15:07:33 +0100

    autoloader

--- index.php
+++ index.php
@@ -4,17 +4,8 @@
 require 'class/Slim/Slim.php';
 require 'class/test.php';
 require 'class/bdd.php';
-require 'modele/loginValidator.php';
-require 'modele/SignInValidator.php';
-require 'modele/logout.php';
-require 'modele/Match.php';
-require 'modele/chat.php';
-require 'modele/chatValidator.php';
-require 'modele/Picture.php';
-require 'modele/getProfil.php';
-require 'modele/ModifProfilValidator.php';
-require 'modele/ModifProfilMoreValidator.php';
-require 'modele/userProfil.php';
+require_once ("/autoload.inc.php");
+
 session_start();
 \Slim\Slim::registerAutoloader();
 
--- modele/getProfil.php
+++ modele/getProfil.php
@@ -1,4 +1,6 @@
 <?php
+
+
 class getProfil {
   private $tabInformations;
   private $tabMoreInformations;
